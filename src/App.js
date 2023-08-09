57f6f8807deeb6046f8837fa6d7f20151be00c3d
Osiris8 <miganosiris8@gmaaail.com>
2023-08-09 11:12:06 +0200

    orgnisation App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [userId, setUserId] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto h-full w-full flex flex-col justify-center items-center">
+      <input
+        type="text"
+        placeholder="Entrez votre nom"
+        className="p-2 rounded-md border-2 border-black w-96 m-8"
+        onChange={(e) => setUserId(e.target.value)}
+      />
+      <div className="p-2 rounded-md border-2 shadow-sm w-96 m-8">{userId}</div>
     </div>
   );
 }
