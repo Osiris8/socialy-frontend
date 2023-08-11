0c7f7b8d5a907f1abb3429fef4b356abf849f3ae
Osiris8 <miganosiris8@gmaaail.com>
2023-08-11 19:23:17 +0200

    to manage like and dislike post

--- src/components/LikePost.js
+++ src/components/LikePost.js
@@ -1,8 +1,48 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function LikePost({ post, userId }) {
+  const [userLiked, setUserLiked] = useState(false);
+
+  useEffect(() => {
+    if (post.likers) {
+      if (post.likers.includes(userId)) {
+        setUserLiked(true);
+      } else {
+        setUserLiked(false);
+      }
+      console.log(userLiked);
+    }
+  }, [userId, post.likers, userLiked]);
+
+  const likePost = () => {
+    axios.patch("http://localhost:5000/start/like-post/" + post._id, {
+      userId: userId,
+    });
+    setUserLiked(true);
+  };
+
+  const dislikePost = () => {
+    axios.patch("http://localhost:5000/start/dislike-post/" + post._id, {
+      userId: userId,
+    });
+    setUserLiked(false);
+  };
+
   return (
     <div>
-      <h1>Post like</h1>
+      <p>
+        <span className="p-2">{post.likers ? post.likers.length : 0}</span>
+        {userLiked ? (
+          <span onClick={() => dislikePost()}>
+            <i className="fa fa-heart text-indigo-500" aria-hidden="true"></i>
+          </span>
+        ) : (
+          <span onClick={() => likePost()}>
+            <i className="fa fa-heart text-gray-400" aria-hidden="true"></i>
+          </span>
+        )}
+      </p>
     </div>
   );
 }
