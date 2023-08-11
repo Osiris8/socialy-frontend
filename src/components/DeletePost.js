b922b82092f5e1e27b3131fbeb9d46257cb85547
Osiris8 <miganosiris8@gmaaail.com>
2023-08-11 19:07:53 +0200

    add DeletePost componen for delete post of user

---
+++ src/components/DeletePost.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+function DeletePost({ postId }) {
+  const handleDelete = () => {
+    axios.delete("http://localhost:5000/start/" + postId);
+  };
+  return (
+    <span
+      onClick={() => {
+        handleDelete();
+        alert("Votre post a bien été supprimé");
+      }}
+      className="text-red-500"
+    >
+      <i class="fa fa-trash text-red-500" aria-hidden="true"></i>
+    </span>
+  );
+}
+export default DeletePost;
