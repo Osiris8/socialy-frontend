6d91de44431ae66a7b0ff0b49a588a983754b10f
Osiris8 <miganosiris8@gmaaail.com>
2023-08-13 17:47:24 +0200

    add getPosts reducer in Threads.js

--- src/components/Threads.js
+++ src/components/Threads.js
@@ -1,20 +1,20 @@
-import axios from "axios";
 import React, { useEffect } from "react";
 import Post from "./Post";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../feature/post.slice";
 function Threads() {
-  const [posts, setPosts] = React.useState([]);
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.postsData);
   useEffect(() => {
-    axios.get(`http://localhost:5000/start/`).then((res) => {
-      setPosts(res.data);
-    });
-  }, []);
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <div className="container">
-      {posts
-        .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
-        .map((post) => (
-          <Post key={post._id} post={post} />
-        ))}
+      {posts &&
+        posts
+          .slice()
+          .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
+          .map((post) => <Post key={post._id} post={post} />)}
     </div>
   );
 }
